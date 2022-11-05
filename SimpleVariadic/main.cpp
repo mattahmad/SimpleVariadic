cf4574aaf76e8f4d9076d31ee153a434c91dcf8f
matt.a@myplanet.com <matt.a@myplanet.com>
2022-11-05 16:52:35 -0400

    Initial Commit

---
+++ SimpleVariadic/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SimpleVariadic
+//
+//  Created by Matt Ahmad on 2022-11-05.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
